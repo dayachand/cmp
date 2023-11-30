a9d47195891ca0538db3ec9d25eefef84a49ad90
dayachand patel <dayachand.patel@systematixindia.com>
2023-11-30 15:52:40 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CommonButton from './CommonButton';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <CommonButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CommonButton.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function CommonButton() {
+  return (
+    <div className="App">
+      <button type="button">Send</button>
+    </div>
+  );
+}
+
+export default CommonButton;
